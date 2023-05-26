138b003b902add7603e97408bcdc65db6cb03e98
SoWhat <dssergeev123@gmail.com>
2023-05-26 11:53:06 +0300

    Create EntryPoint,BaseController and Tools

---
+++ Assets/Scripts/MyTowerDefence/EntryPoint.cs
@@ -0,0 +1,19 @@
+using Profile;
+using UnityEngine;
+
+public class EntryPoint : MonoBehaviour
+{
+    private const GameState InitialState = GameState.Start;
+
+    private MainController _mainController;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
